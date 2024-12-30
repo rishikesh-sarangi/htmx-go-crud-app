f2bd911ffacb4387e4346c6b7345c3918a7cf0fb
Rishikesh Sarangi <rishikeshsarangi70@gmail.com>
2024-12-30 12:37:01 +0530

    setup done

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import {resolve} from "path";
+import { defineConfig } from "vite";
+
+
+export default defineConfig({
+    build : {
+        lib: {
+            entry: [resolve(__dirname, "src/htmx.js")],
+            formats: ["es"],
+            name: "[name]",
+            fileName: "[name]"
+        },
+        outDir: "static",
+        emptyOutDir: false
+    }
+});
